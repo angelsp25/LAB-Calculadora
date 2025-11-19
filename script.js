3b96a3c255bdeebc97fe6063752f5045347e3169
Angel Sanchez <angel.2525@live.com.mx>
2025-11-19 16:27:23 -0700

    fix: se corrigió un error relacionado con la función que covierte a flotante el string

--- script.js
+++ script.js
@@ -2,41 +2,46 @@
 
 //Inicio
 
-function suma(num1, num2) //Función para sumar 2 números
+function convertirFlotante(num1, num2)
 {
-    convertirFlotante(num1, num2);
+    num1 = parseFloat(num1); //La variable num1 se parsea a flotante
+    num2 = parseFloat(num2); //La variable num2 se parsea a flotante
+    
+    return [num1, num2];
+    //return [parseFloat(num1), parseFloat(num2)];
+}
+
+function suma(num1, num2)//Función para sumar 2 números
+{
+    [num1, num2] = convertirFlotante(num1, num2);
     
     return num1 + num2; //Se regresa la suma de las variables num1 + num2
 }
 
 function resta(num1, num2)//Función para restar 2 números
 {
-    convertirFlotante(num1, num2);
+    [num1, num2] = convertirFlotante(num1, num2);
     
     return num1 - num2; //Se regresa la resta de las variables num1 - num2
 }
 
-function dividir(num1, num2)
+function dividir(num1, num2) //Función para dividir 2 números
 {
-    convertirFlotante(num1, num2);
+    [num1, num2] = convertirFlotante(num1, num2);
     
     return num1 / num2; //Se regresa la división de las variables num1 / num2
 }
 
-function multipicar(num1, num2)
+function multipicar(num1, num2) //Función para multiplicar 2 números
 {
-    convertirFlotante(num1, num2);
+    [num1, num2] = convertirFlotante(num1, num2);
     
     return num1 * num2; //Se regresa la multiplicación de las variables num1 * num2
 }
 
-function convertirFlotante(num1, num2)
-{
-    num1 = parseFloat(num1); //La variable num1 se parsea a flotante
-    num2 = parseFloat(num2); //La variable num2 se parsea a flotante
-}
 
-let resultadoSuma = suma("11", 87); //Ejemplo utilizando la función suma con string y número
+
+let resultadoSuma = suma("12", 87); //Ejemplo utilizando la función suma con string y número
 console.log(resultadoSuma);
 
 let resultadoResta = resta(95, "43"); //Ejemplo utilizando la función resta con número y string
