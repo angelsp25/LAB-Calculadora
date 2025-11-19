cdec8a6201f5444d670e2f55689a924137815228
Angel Sanchez <angel.2525@live.com.mx>
2025-11-19 12:39:22 -0700

    feat: se agregó una sección de código al archivo script.js con las funciones básicas de una calculadora

---
+++ script.js
@@ -0,0 +1,23 @@
+//Inicio
+
+function suma(a, b) //Función para sumar 2 números
+{
+    return a + b; //Se regresa la suma de las variables a + b
+}
+
+function resta(a, b)//Función para restar 2 números
+{
+    return a - b; //Se regresa la resta de las variables a - b
+}
+
+function dividir(a, b)
+{
+    return a / b; //Se regresa la división de las variables a / b
+}
+
+function multipicar(a, b)
+{
+    return a * b; //Se regresa la multiplicación de las variables a * b
+}
+
+//Final
